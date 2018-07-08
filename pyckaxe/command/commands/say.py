6ce17ce64671de5569856ad23c1a8d22c90a76be
Arcensoth <arcensoth@gmail.com>
2018-07-08 00:39:14 -0400

    Finish say command

--- pyckaxe/command/commands/say.py
+++ pyckaxe/command/commands/say.py
@@ -1,5 +1,15 @@
-from pyckaxe.command.abc.command import CommandLiteral
+from pyckaxe.command.abc.command import CommandArguments, CommandLiteral
 
 
 class SayCommand(CommandLiteral):
     _LITERAL = 'say'
+
+    def __call__(self, message: str = None) -> 'SayMessageCommand':
+        return SayMessageCommand(parent=self, args=(message,))
+
+    def message(self, message: str) -> 'SayMessageCommand':
+        return SayMessageCommand(parent=self, args=(message,))
+
+
+class SayMessageCommand(CommandArguments):
+    pass
