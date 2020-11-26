132f3efcd5290e6bb677cb9381507e1788caab45
Arcensoth <arcensoth@gmail.com>
2020-11-26 11:49:48 -0500

    Make Block de/serializable

--- pyckaxe/block.py
+++ pyckaxe/block.py
@@ -3,15 +3,36 @@ from typing import Iterable, Optional
 
 from nbtlib import tag
 
+from pyckaxe.abc.serializable import Serializable
 from pyckaxe.block_state import BlockState
 from pyckaxe.command.abc.command_token import CommandToken
+from pyckaxe.utils.nbt import to_nbt
 
 
 @dataclass
-class Block(CommandToken):
+class Block(CommandToken, Serializable):
     name: str
     state: Optional[BlockState] = None
     data: Optional[tag.Compound] = None
+
+    @staticmethod
+    def deserialize(raw: dict) -> "Block":
+        assert isinstance(raw, dict)
+        name = raw["block"]
+        raw_state = raw.get("state")
+        state = BlockState(raw_state) if raw_state is not None else None
+        raw_data = raw.get("data")
+        data = to_nbt(raw_data) if raw_data is not None else None
+        return Block(name=name, state=state, data=data)
+
+    # @implements Serializable
+    def serialize(self) -> dict:
+        # FIXME Shouldn't `data` be a dict as well?
+        return {
+            "name": self.name,
+            "state": self.state.serialize(),
+            "data": self.data.snbt(),
+        }
 
     def _command_stringify(self) -> Iterable[str]:
         yield self.name
