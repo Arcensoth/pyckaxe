3916b0b6b308688cb37f1bd80a399d66cfe2a70e
Arcensoth <arcensoth@gmail.com>
2020-11-26 11:49:39 -0500

    Add missing deserialize method to Serializable ABCs

--- pyckaxe/abc/serializable.py
+++ pyckaxe/abc/serializable.py
@@ -1,16 +1,24 @@
 from abc import ABC, abstractmethod
-from typing import Union
+from typing import Any, Union
 
 SerializedValue = Union[int, float, str, dict, list]
 
 
 class Serializable(ABC):
+    @staticmethod
+    def deserialize(raw: dict) -> Any:
+        ...
+
     @abstractmethod
     def serialize(self) -> SerializedValue:
         ...
 
 
 class AsyncSerializable(ABC):
+    @staticmethod
+    async def deserialize(raw: dict) -> Any:
+        ...
+
     @abstractmethod
     async def serialize(self) -> SerializedValue:
         ...
