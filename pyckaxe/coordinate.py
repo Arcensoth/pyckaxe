5c5d7b186a251866ff96462b2ee7229a7e377c91
Arcensoth <arcensoth@gmail.com>
2020-11-30 22:32:54 -0500

    Add more dunder methods to Pos/Coord

--- pyckaxe/coordinate.py
+++ pyckaxe/coordinate.py
@@ -4,9 +4,6 @@ from pyckaxe.command.abc.command_token import CommandToken
 
 
 class Coordinate(CommandToken):
-    def __init__(self, value: float):
-        self.value: float = float(value)
-
     @staticmethod
     def from_any(value: Any) -> "Coordinate":
         if isinstance(value, Coordinate):
@@ -16,6 +13,27 @@ class Coordinate(CommandToken):
         if isinstance(value, int):
             return AbsoluteCoordinate(float(value))
         raise ValueError(f"Value cannot be converted to {Coordinate.__name__}: {value}")
+
+    def __init__(self, value: float):
+        self.value: float = float(value)
+
+    def __repr__(self) -> str:
+        return str(self)
+
+    def __str__(self) -> str:
+        return self.command_stringify()
+
+    def __hash__(self) -> int:
+        return hash(self.value)
+
+    def __eq__(self, other: Any) -> bool:
+        return other.__eq__(self.value)
+
+    def __lt__(self, other: Any) -> bool:
+        return other.__lt__(self.value)
+
+    def __gt__(self, other: Any) -> bool:
+        return other.__gt__(self.value)
 
     def __bool__(self) -> bool:
         return bool(self.value)
@@ -32,6 +50,17 @@ class AbsoluteCoordinate(Coordinate):
         c = Coordinate.from_any(other)
         return AbsoluteCoordinate(self.value + c.value)
 
+    def __sub__(self, other: Any) -> "AbsoluteCoordinate":
+        c = Coordinate.from_any(other)
+        return AbsoluteCoordinate(self.value - c.value)
+
+    def __mul__(self, other: Any) -> "AbsoluteCoordinate":
+        c = Coordinate.from_any(other)
+        return AbsoluteCoordinate(self.value * c.value)
+
+    def __neg__(self) -> "AbsoluteCoordinate":
+        return AbsoluteCoordinate(-self.value)
+
     # @implements CommandToken
     def command_stringify(self) -> str:
         return f"{self.value:g}"
@@ -42,6 +71,17 @@ class RelativeCoordinate(Coordinate):
         c = Coordinate.from_any(other)
         return RelativeCoordinate(self.value + c.value)
 
+    def __sub__(self, other: Any) -> "RelativeCoordinate":
+        c = Coordinate.from_any(other)
+        return RelativeCoordinate(self.value - c.value)
+
+    def __mul__(self, other: Any) -> "RelativeCoordinate":
+        c = Coordinate.from_any(other)
+        return RelativeCoordinate(self.value * c.value)
+
+    def __neg__(self) -> "RelativeCoordinate":
+        return RelativeCoordinate(-self.value)
+
     # @implements CommandToken
     def command_stringify(self) -> str:
         return f"~{self.value:g}"
@@ -51,6 +91,17 @@ class LocalCoordinate(Coordinate):
     def __add__(self, other: Any) -> "LocalCoordinate":
         c = Coordinate.from_any(other)
         return LocalCoordinate(self.value + c.value)
+
+    def __sub__(self, other: Any) -> "LocalCoordinate":
+        c = Coordinate.from_any(other)
+        return LocalCoordinate(self.value - c.value)
+
+    def __mul__(self, other: Any) -> "LocalCoordinate":
+        c = Coordinate.from_any(other)
+        return LocalCoordinate(self.value * c.value)
+
+    def __neg__(self) -> "LocalCoordinate":
+        return LocalCoordinate(-self.value)
 
     # @implements CommandToken
     def command_stringify(self) -> str:
--- pyckaxe/position.py
+++ pyckaxe/position.py
@@ -6,11 +6,6 @@ from pyckaxe.utils.fields import DEFAULT, get_field
 
 
 class Position(CommandToken):
-    def __init__(self, x: Any, y: Any, z: Any):
-        self.x: Coordinate = Coordinate.from_any(x)
-        self.y: Coordinate = Coordinate.from_any(y)
-        self.z: Coordinate = Coordinate.from_any(z)
-
     @staticmethod
     def from_field(raw: dict, field: str, default=DEFAULT) -> "Position":
         raw_position = get_field(
@@ -19,14 +14,33 @@ class Position(CommandToken):
         position = Position(*raw_position)
         return position
 
+    def __init__(self, x: Any, y: Any, z: Any):
+        self.x: Coordinate = Coordinate.from_any(x)
+        self.y: Coordinate = Coordinate.from_any(y)
+        self.z: Coordinate = Coordinate.from_any(z)
+
+    def __repr__(self) -> str:
+        return str(self)
+
+    def __str__(self) -> str:
+        return self.command_stringify()
+
+    def __hash__(self) -> int:
+        return hash(self.x) + hash(self.y) + hash(self.z)
+
+    def __eq__(self, other: object) -> bool:
+        if isinstance(other, Position):
+            return (other.x == self.x) and (other.y == self.y) and (other.z == self.z)
+        return False
+
     def __bool__(self) -> bool:
         return bool(self.x) or bool(self.y) or bool(self.z)
 
     def __invert__(self) -> "Position":
-        rx = ~self.x
-        ry = ~self.y
-        rz = ~self.z
-        return Position(rx, ry, rz)
+        return Position(~self.x, ~self.y, ~self.z)
+
+    def __neg__(self) -> "Position":
+        return Position(-self.x, -self.y, -self.z)
 
     def __add__(self, other: Any) -> "Position":
         if isinstance(other, Position):
@@ -37,6 +51,15 @@ class Position(CommandToken):
 
     def unpack(self) -> Tuple[Coordinate, Coordinate, Coordinate]:
         return self.x, self.y, self.z
+
+    def unpack_floats(self) -> Tuple[float, float, float]:
+        return self.x.value, self.y.value, self.z.value
+
+    def unpack_ints(self) -> Tuple[int, int, int]:
+        return int(self.x.value), int(self.y.value), int(self.z.value)
+
+    def exceeds(self, other: "Position") -> bool:
+        return (self.x > other.x) and (self.y > other.y) and (self.z > other.z)
 
     # @implements CommandToken
     def command_stringify(self) -> str:
