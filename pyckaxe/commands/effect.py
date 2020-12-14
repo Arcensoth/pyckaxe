268ace2847e1b5c4337d4d17cc91838e2c0a35ae
Arcensoth <arcensoth@gmail.com>
2020-12-14 02:18:31 -0500

    Experiment with an alternate command namespacing scheme

--- pyckaxe/commands/effect.py
+++ pyckaxe/commands/effect.py
@@ -1,88 +1,75 @@
 from pyckaxe.command.abc.command import CommandArgument, CommandLiteral
 from pyckaxe.types import CommandTarget, Effect
 
+# NOTE This command uses a different form of namespacing: nested classes.
+# This results in class names with sane lengths, but conversely insane indentation. It also has the
+# (arguably negligible) side-effect of polluting the namespace with sub-classes.
+
 
 class EffectCommand(CommandLiteral):
     _LITERAL = "effect"
 
-    @property
-    def clear(self) -> "EffectClearCommand":
-        return EffectClearCommand(self)
+    class _Clear(CommandLiteral):
+        _LITERAL = "clear"
+
+        class _Targets(CommandArgument):
+            class _Effect(CommandArgument):
+                pass
+
+            def effect(self, effect: Effect) -> _Effect:
+                return self._Effect(self, effect)
+
+        def __call__(self, targets: CommandTarget, effect: Effect) -> _Targets._Effect:
+            return self.targets(targets).effect(effect)
+
+        def targets(self, targets: CommandTarget) -> _Targets:
+            return self._Targets(self, targets)
 
     @property
-    def give(self) -> "EffectGiveCommand":
-        return EffectGiveCommand(self)
+    def clear(self) -> _Clear:
+        return self._Clear(self)
 
+    class _Give(CommandLiteral):
+        _LITERAL = "give"
 
-class EffectClearCommand(CommandLiteral):
-    _LITERAL = "clear"
+        class _Targets(CommandArgument):
+            class _Effect(CommandArgument):
+                class _Seconds(CommandArgument):
+                    class _Amplifier(CommandArgument):
+                        class _HideParticles(CommandArgument):
+                            pass
 
-    def __call__(
-        self, targets: CommandTarget, effect: Effect
-    ) -> "EffectClearTargetsEffectCommand":
-        return self.targets(targets).effect(effect)
+                        def hide_particles(self, hide_particles: bool) -> _HideParticles:
+                            return self._HideParticles(self, hide_particles)
 
-    def targets(self, targets: CommandTarget) -> "EffectClearTargetsCommand":
-        return EffectClearTargetsCommand(self, targets)
+                    def amplifier(self, amplifier: int) -> _Amplifier:
+                        return self._Amplifier(self, amplifier)
 
+                def seconds(self, seconds: int) -> _Seconds:
+                    return self._Seconds(self, seconds)
 
-class EffectClearTargetsCommand(CommandArgument):
-    def effect(self, effect: Effect) -> "EffectClearTargetsEffectCommand":
-        return EffectClearTargetsEffectCommand(self, effect)
+            def effect(self, effect: Effect) -> _Effect:
+                return self._Effect(self, effect)
 
+        def __call__(
+            self,
+            targets: CommandTarget,
+            effect: Effect,
+            seconds: int,
+            amplifier: int,
+            hide_particles: bool,
+        ) -> _Targets._Effect._Seconds._Amplifier._HideParticles:
+            return (
+                self.targets(targets)
+                .effect(effect)
+                .seconds(seconds)
+                .amplifier(amplifier)
+                .hide_particles(hide_particles)
+            )
 
-class EffectClearTargetsEffectCommand(CommandArgument):
-    pass
+        def targets(self, targets: CommandTarget) -> _Targets:
+            return self._Targets(self, targets)
 
-
-class EffectGiveCommand(CommandLiteral):
-    _LITERAL = "give"
-
-    def __call__(
-        self,
-        targets: CommandTarget,
-        effect: Effect,
-        seconds: int,
-        amplifier: int,
-        hide_particles: bool,
-    ) -> "EffectGiveTargetsEffectSecondsAmplifierHideParticlesCommand":
-        return (
-            self.targets(targets)
-            .effect(effect)
-            .seconds(seconds)
-            .amplifier(amplifier)
-            .hide_particles(hide_particles)
-        )
-
-    def targets(self, targets: CommandTarget) -> "EffectGiveTargetsCommand":
-        return EffectGiveTargetsCommand(self, targets)
-
-
-class EffectGiveTargetsCommand(CommandArgument):
-    def effect(self, effect: Effect) -> "EffectGiveTargetsEffectCommand":
-        return EffectGiveTargetsEffectCommand(self, effect)
-
-
-class EffectGiveTargetsEffectCommand(CommandArgument):
-    def seconds(self, seconds: int) -> "EffectGiveTargetsEffectSecondsCommand":
-        return EffectGiveTargetsEffectSecondsCommand(self, seconds)
-
-
-class EffectGiveTargetsEffectSecondsCommand(CommandArgument):
-    def amplifier(
-        self, amplifier: int
-    ) -> "EffectGiveTargetsEffectSecondsAmplifierCommand":
-        return EffectGiveTargetsEffectSecondsAmplifierCommand(self, amplifier)
-
-
-class EffectGiveTargetsEffectSecondsAmplifierCommand(CommandArgument):
-    def hide_particles(
-        self, hide_particles: bool
-    ) -> "EffectGiveTargetsEffectSecondsAmplifierHideParticlesCommand":
-        return EffectGiveTargetsEffectSecondsAmplifierHideParticlesCommand(
-            self, hide_particles
-        )
-
-
-class EffectGiveTargetsEffectSecondsAmplifierHideParticlesCommand(CommandArgument):
-    pass
+    @property
+    def give(self) -> _Give:
+        return self._Give(self)
