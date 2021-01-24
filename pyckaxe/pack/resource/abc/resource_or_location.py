6a40a5903f1d9a155440ca21eff46619b3b7bbc5
Arcensoth <arcensoth@gmail.com>
2021-01-23 23:21:30 -0500

    Overhaul ResourceOrLocation

--- pyckaxe/pack/resource/abc/resource_or_location.py
+++ pyckaxe/pack/resource/abc/resource_or_location.py
@@ -1,6 +1,7 @@
 from typing import Any, Coroutine, Generic, Optional, Type, TypeVar
 
 from pyckaxe.pack.pack_context import PackContext
+from pyckaxe.pack.resource.abc.located_resource import LocatedResource
 from pyckaxe.pack.resource.abc.resource import Resource
 from pyckaxe.pack.resource.abc.resource_location import ResourceLocation
 from pyckaxe.utils.fields import DEFAULT, get_field
@@ -16,22 +17,38 @@ class ResourceOrLocation(Generic[ResourceType, ResourceLocationType]):
         resource_location_class: Type[ResourceLocation] = ResourceLocation
 
     @classmethod
+    async def deserialize_resource_location(
+        cls: Type[ResourceOrLocationType], raw: str
+    ) -> ResourceOrLocationType:
+        resource_location = cls.Meta.resource_location_class.from_string(raw)
+        resource_or_location = cls(resource_location=resource_location)
+        return resource_or_location
+
+    @classmethod
+    async def deserialize_resource(
+        cls: Type[ResourceOrLocationType], raw: Any
+    ) -> ResourceOrLocationType:
+        resource = await cls.Meta.resource_class.deserialize(raw)
+        resource_or_location = cls(resource=resource)
+        return resource_or_location
+
+    @classmethod
+    async def deserialize(cls: Type[ResourceOrLocationType], raw: Any) -> ResourceOrLocationType:
+        # A string is assumed to be a resource location.
+        if isinstance(raw, str):
+            return await cls.deserialize_resource_location(raw)
+        # Anything else is assumed to be a serialized resource.
+        return await cls.deserialize_resource(raw)
+
+    @classmethod
     async def from_field(
         cls: Type[ResourceOrLocationType], raw: dict, field: str, default=DEFAULT
-    ) -> ResourceOrLocationType:
+    ) -> Optional[ResourceOrLocationType]:
         # Extract the field from the raw data.
-        raw_resource_or_location = get_field(raw, field, default=default)
-        # A string is assumed to be a resource location.
-        if isinstance(raw_resource_or_location, str):
-            resource_location = cls.Meta.resource_location_class.from_string(
-                raw_resource_or_location
-            )
-            resource_or_location = cls(resource_location=resource_location)
-        # Anything else is assumed to be a serialized resource.
-        else:
-            resource = await cls.Meta.resource_class.deserialize(raw_resource_or_location)
-            resource_or_location = cls(resource=resource)
-        return resource_or_location
+        raw_value = get_field(raw, field, default=default)
+        # If it's non-null, then attempt to deserialize it.
+        if raw_value is not None:
+            return await cls.deserialize(raw_value)
 
     def __init__(
         self,
@@ -50,20 +67,54 @@ class ResourceOrLocation(Generic[ResourceType, ResourceLocationType]):
         self.resource_location: Optional[ResourceLocationType] = resource_location
 
     def __call__(self, pack_context: PackContext) -> Coroutine[Any, Any, ResourceType]:
-        """
-        Mimics the call resolution of `ResourceLocation` so that `ResourceOrLocation` can be used
-        in the same way.
-        """
+        """ Mimics the call resolution of [ResourceLocation] to be used in the same way. """
         return self.resolve_resource(pack_context)
 
     async def resolve_resource(self, pack_context: PackContext) -> ResourceType:
         """
-        Returns the contained `ResourceType`, if available. Otherwise, resolves the contained
-        `ResourceLocationType` into a `ResourceType` and returns that.
+        Returns the resolved [Resource], be it inline or referred.
+
+        If this [ResourceOrLocation] contains a [Resource], it is returned directly.
+
+        Otherwise this [ResourceOrLocation] contains a [ResourceLocation], in which case it is
+        asynchronously resolved and returned.
         """
-        # If we already have a `Resource`, just return that.
         if self.resource is not None:
             return self.resource
-        # Otherwise we must have a `ResourceLocation` that needs to be resolved.
+        assert self.resource_location is not None
         resource = await self.resource_location.resolve_resource(pack_context)
         return resource
+
+    def location_or_inline(self, inline_location: ResourceLocationType) -> ResourceLocationType:
+        """
+        Returns either the contained [ResourceLocaton] or the given [inline_location].
+
+        If this [ResourceOrLocation] contains a [Resource], the given `inline_location` is returned.
+
+        Otherwise this [ResourceOrLocation] contains a [ResourceLocation], in which case this is
+        returned instead.
+
+        Use this to replace code segments that need to check whether to produce an inline resource.
+        """
+        if self.resource is not None:
+            return inline_location
+        assert self.resource_location is not None
+        return self.resource_location
+
+    async def resolve_and_locate_resource(
+        self, ctx: PackContext, inline_location: ResourceLocationType
+    ) -> LocatedResource[ResourceType, ResourceLocationType]:
+        """
+        Returns a [LocatedResource] with the resolved [Resource].
+
+        If this [ResourceOrLocation] contains a [Resource], it is paired with the given
+        [inline_location].
+
+        Otherwise this [ResourceOrLocation] contains a [ResourceLocation], in which case it is
+        paired with its own asynchronous resolution.
+        """
+        if self.resource is not None:
+            return LocatedResource(self.resource, inline_location)
+        assert self.resource_location is not None
+        resolved_resource = await self.resource_location(ctx)
+        return LocatedResource(resolved_resource, self.resource_location)
